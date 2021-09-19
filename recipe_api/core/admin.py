e918a95423db94cd8163acc34a3e4cc5f725f538
Junxian Qi <junxian.j.qi@gmail.com>
2021-09-19 10:43:22 +0800

    Register custom user model in admin site

--- recipe_api/core/admin.py
+++ recipe_api/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ('id',)
+    list_display = ('email', 'name')
