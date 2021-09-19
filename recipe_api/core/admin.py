5a7ec070d39c9cbfead69d90f93f76e7e6ae024b
Junxian Qi <junxian.j.qi@gmail.com>
2021-09-19 14:51:05 +0800

    Add more fields in UserAdmin

--- recipe_api/core/admin.py
+++ recipe_api/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from . import models
 
@@ -8,3 +9,11 @@ from . import models
 class UserAdmin(BaseUserAdmin):
     ordering = ('id',)
     list_display = ('email', 'name')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name',)}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser'),
+        }),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
