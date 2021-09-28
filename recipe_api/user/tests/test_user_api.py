9273b9339d57b3cd1986832caa6b560fa54d5fd2
Junxian Qi <junxian.j.qi@gmail.com>
2021-09-28 12:57:04 +0800

    Add creating a user that already exists test

--- recipe_api/user/tests/test_user_api.py
+++ recipe_api/user/tests/test_user_api.py
@@ -26,3 +26,11 @@ class PublicUserApiTests(TestCase):
         user = get_user_model().objects.get(**response.data)
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', response.data)
+
+    def test_user_exists(self):
+        """Test creating a user that already exists is failed"""
+        payload = {'email': 'testexists@test.com', 'password': 'test-exists', 'name': 'Test Exists'}
+        create_user(**payload)
+        response = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
