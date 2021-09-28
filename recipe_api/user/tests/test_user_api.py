91f5728421b82f4d0626a26249a614f6b861221d
Junxian Qi <junxian.j.qi@gmail.com>
2021-09-28 12:52:14 +0800

    Add creating a user with valid payload test

---
+++ recipe_api/user/tests/test_user_api.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test public user APIs"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user(self):
+        """Test creating a user with valid payload is successful"""
+        payload = {'email': 'test@test.com', 'password': 'Password', 'name': 'Test User'}
+        response = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**response.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', response.data)
