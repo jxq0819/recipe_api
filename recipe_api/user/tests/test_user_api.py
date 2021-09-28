3d03e5c78ddfac8d8079c50fe4c7c27acc86c13e
Junxian Qi <junxian.j.qi@gmail.com>
2021-09-28 13:08:15 +0800

    Add creating a user with short password test

--- recipe_api/user/tests/test_user_api.py
+++ recipe_api/user/tests/test_user_api.py
@@ -34,3 +34,12 @@ class PublicUserApiTests(TestCase):
         response = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_short(self):
+        """Test password must be more than 5 characters"""
+        payload = {'email': 'testshort@test.com', 'password': 'pd', 'name': 'Test ShortPassword'}
+        response = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        self.assertFalse(user_exists)
