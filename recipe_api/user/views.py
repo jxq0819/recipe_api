bb3542093437d6dedd34753db5eac3d4b6509208
Junxian Qi <junxian.j.qi@gmail.com>
2021-09-29 23:19:41 +0800

    Add API view of creating a new token for the user

--- recipe_api/user/views.py
+++ recipe_api/user/views.py
@@ -1,8 +1,16 @@
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """API view of creating a new user"""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """API view of creating a token for the user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
