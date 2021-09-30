45fca4e0bfb784fa40fd02ecd362de2e82061eff
Junxian Qi <junxian.j.qi@gmail.com>
2021-09-30 15:28:24 +0800

    Add API view of retrieving and updating user

--- recipe_api/user/views.py
+++ recipe_api/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -14,3 +14,14 @@ class CreateTokenView(ObtainAuthToken):
     """API view of creating a token for the user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """API view of retrieving and updating the user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve the authenticated user"""
+        return self.request.user
