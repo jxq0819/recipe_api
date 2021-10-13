d739a00aa2ee919225c1222394b23f35a5d15c29
Junxian Qi <junxian.j.qi@gmail.com>
2021-10-13 14:22:39 +0800

    Add creating a new tag with invalid payload test

--- recipe_api/recipe/tests/test_tags_api.py
+++ recipe_api/recipe/tests/test_tags_api.py
@@ -62,3 +62,10 @@ class PrivateTagsApiTest(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(tag_exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        response = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
