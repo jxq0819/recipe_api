070e42f912925c596bff0ce2e73a5f50db26bbfd
Junxian Qi <junxian.j.qi@gmail.com>
2021-10-13 11:04:23 +0800

    Add API view set of managing tags

--- recipe_api/recipe/views.py
+++ recipe_api/recipe/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from core.models import Tag
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import TagSerializer
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """API view set for managing tags"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        """Return tags for the current authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
