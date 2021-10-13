6347b4a922a2cdb0fd6f09a99d41acbf988382c6
Junxian Qi <junxian.j.qi@gmail.com>
2021-10-13 17:45:42 +0800

    Add listing ingredients test

--- recipe_api/recipe/tests/test_ingredients_api.py
+++ recipe_api/recipe/tests/test_ingredients_api.py
@@ -1,3 +1,5 @@
+from core.models import Ingredient
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -17,3 +19,24 @@ class PublicIngredientsApiTest(TestCase):
         response = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsApiTest(TestCase):
+    """Test private ingredients APIs"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(email='test@test.com', password='testIngredient')
+        self.client.force_authenticate(self.user)
+
+    def test_list_ingredients(self):
+        """Test listing ingredients"""
+        Ingredient.objects.create(user=self.user, name='Spinach')
+        Ingredient.objects.create(user=self.user, name='Broccoli')
+
+        response = self.client.get(INGREDIENTS_URL)
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
