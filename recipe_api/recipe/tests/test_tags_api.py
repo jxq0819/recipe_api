1fee8c6554aa28d78dc4a05f08f037037f0c0c88
Junxian Qi <junxian.j.qi@gmail.com>
2021-10-13 10:03:42 +0800

    Add listing tags test

--- recipe_api/recipe/tests/test_tags_api.py
+++ recipe_api/recipe/tests/test_tags_api.py
@@ -1,3 +1,5 @@
+from core.models import Tag
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -17,3 +19,24 @@ class PublicTagsApiTest(TestCase):
         response = self.client.get(TAGS_URL)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTest(TestCase):
+    """Test private tags APIs"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(email='test@test.com', password='tesTTest321')
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_list_tags(self):
+        """Test listing tags"""
+        Tag.objects.create(user=self.user, name='Low fat')
+        Tag.objects.create(user=self.user, name='Gluten free')
+
+        response = self.client.get(TAGS_URL)
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
