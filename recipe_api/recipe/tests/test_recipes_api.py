a7726f739755b5756c55485435f71a469201ccea
Junxian Qi <junxian.j.qi@gmail.com>
2021-10-16 10:57:08 +0800

    Add listing recipes test

--- recipe_api/recipe/tests/test_recipes_api.py
+++ recipe_api/recipe/tests/test_recipes_api.py
@@ -1,4 +1,5 @@
 from core.models import Recipe
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -26,3 +27,24 @@ class PublicRecipesTest(TestCase):
         response = self.client.get(RECIPES_URL)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateRecipesTest(TestCase):
+    """Test private recipes APIs"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(email='test@test.com', password='TestRecipe')
+        self.client.force_authenticate(self.user)
+
+    def test_list_recipes(self):
+        """Test listing recipes"""
+        Recipe.objects.create(user=self.user, title='Sample recipe', time_minutes=10, price=5.00)
+        Recipe.objects.create(user=self.user, title='Sample recipe', time_minutes=10, price=5.00)
+
+        response = self.client.get(RECIPES_URL)
+        recipes = Recipe.objects.all().order_by('id')
+        serializer = RecipeSerializer(recipes, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
